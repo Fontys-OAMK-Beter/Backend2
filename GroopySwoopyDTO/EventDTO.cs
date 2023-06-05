db1f570e111665cbeadcaea71b668f4fc6b4da59
Dijcks,Brian B.M <469729@student.fontys.nl>
2023-06-05 13:30:28 +0200

    eventget fix

--- GroopySwoopyDTO/EventDTO.cs
+++ GroopySwoopyDTO/EventDTO.cs
@@ -9,14 +9,14 @@ namespace GroopySwoopyDTO
     public class EventDTO
     {
         public int Id { get; set; }
-
-        public int GroupId { get; set; }
-
+                
         public DateTime StartTime { get; set; }
 
         public string Description { get; set; }
 
         public string Title { get; set; }
+
+        public int GroupId { get; set; }
 
         public string PictureUrl { get; set; }
     }
